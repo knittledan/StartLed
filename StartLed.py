b74df7145d529eb73ef2792b440d8e0d9dae9056
knittledan <knittledan@gmail.com>
2017-05-14 21:51:35 -0700

    cr

--- StartLed.py
+++ StartLed.py
@@ -3,147 +3,178 @@
 import sys
 from time import sleep
 import pifacedigitalio
-from StartLedArgumentParser import StartLedArgumentParser
+from start_led_argument_parser import StartLedArgumentParser
 
-class StartLed :
+
+class StartLed:
     flashSpeedLimit = None
     pfio = pifacedigitalio.PiFaceDigital()
     piFaceListener = None
-    toggleValue = None
-    flashSpeed = None
-    startLedPin = None
-    flashStop = None
+    toggle_value = None
+    flash_speed = None
+    start_led_pin = None
+    flash_stop = None
     highSpeed = None
     lowSpeed = None
-    
-    def __init__(self, args = dict()) :
-        pifacedigitalio.init()
-        self.startListener(self)
-        self.startLed(self, args)
 
-    
-    def startListener(self) :
-        self.piFaceListener = pifacedigitalio.InputEventListener(chip = self.pfio)
-        self.piFaceListener.register(0, pifacedigitalio.IODIR_BOTH, self.listenerCallback(self))
-        self.piFaceListener.activate()
+    def __init__(self, args: dict
 
-    def listenerCallback(self) :
-        pass
-
-    def startLed(self, args = dict()) :
-        self.setFlashProperties(self, args)
-        self.flashLed(self)
-        self.close(self)
-
-    def transformFlashLedArgsInput(self) :
-        self.startLedPin = int(input("LED number: "))
-        self.toggleValue = int(input("Toggle on (1) or off (0): "))
-        if self.toggleValue == 1 :
-            self.flashSpeed = int(input("Set flash speed (1-10): "))
-            self.flashStop = False
-        else :
-            self.flashSpeed = 0
-            self.flashStop = True
-
-    def transformFlashLedArgumentsMain(self, args) :
-        if not args.speed and not args.stop :
-            args.stop = True
-        self.toggleValue = args.toggle
-        self.flashSpeed = args.speed
-        self.startLedPin = args.led
-        self.flashStop = args.stop
-
-    def toggleLed(self) :
-        #this is the non-oop way
-        #pifacedigitalio.digital_write(pin, i)
-        #output_pins and leds seem to be the same array, leds has methods
-        #self.pfio.output_pins[pin].value = i
-        if self.toggleValue == 0 :
-            self.pfio.leds[self.startLedPin].turn_off()
-        else :
-            self.pfio.leds[self.startLedPin].turn_on()
-            self.pfio.leds[self.startLedPin].set_high()
-
-    def setFlashProperties(self, args = dict()) :
-        self.setFlashSpeedProperties(self, args)
-        self.setSleepTime(self)
-
-    def setFlashSpeedProperties(self, args = dict()) :
-        self.setFlashSpeedLimit(self)
-        self.setFlashHighSpeed(self)
-        self.setFlashLowSpeed(self)
-        if not args :
-            self.transformFlashLedArgsInput(self)
-        else :
-            self.transformFlashLedArgsMain(self, args)
-        self.transformFlashSpeed(self)
-
-    def setFlashHighSpeed(self) :
-        self.highSpeed = self.flashSpeedLimit - 1
-
-    def setFlashLowSpeed(self) :
-        self.lowSpeed = 1
-
-    def setFlashSpeedLimit(self) :
-        self.flashSpeedLimit = 11
-
-    def transformFlashSpeed(self) :
-        if self.flashSpeed > self.highSpeed :
-            self.flashSpeed = self.highSpeed
-        elif self.flashSpeed < self.lowSpeed :
-            self.flashSpeed = self.lowSpeed
-
-    def setSleepTime(self) :
-        self.sleepTime = self.flashSpeedLimit - self.flashSpeed
-
-    def flashLed(self) :
-        self.toggleLed(self)
-        if self.flashStop :
-            return
-        sleep(self.sleepTime)
-        self.flipToggle(self)
-        self.flashLed(self)
-
-    def flipToggle(self) :
-        if self.toggleValue == 1 :
-            self.toggleValue = 0
-        else :
-            self.toggleValue = 1
-    
-    def readInput(self) :
-        #pifacedigitalio.digital_read(0)
-        print(se1f.pfio.output_pins[self.startLedPin].value)
-
-    def togglePinPullups(self, pinNumber, pinState) :
-        digital_write_pullup(pinNumber, pinState)
-
-    def close(self) :
-        self.pfio.deinit_board()
-
-    def shutOffAllLeds(self) :
-        self.pfio.output_port.all_off()
+    ):
+    pifacedigitalio.init()
+    self.start_listener()
+    self.start_led(args)
 
 
-def main() :
-    try :
-        args_dict = StartLedArgumentParser.getArgsDict(StartLedArgumentParser)
+def start_listener(self):
+    self.piFaceListener = pifacedigitalio.InputEventListener(chip=self.pfio)
+    self.piFaceListener.register(0, pifacedigitalio.IODIR_BOTH,
+                                 self.listenerCallback())
+    self.piFaceListener.activate()
+
+
+def listenerCallback(self):
+    pass
+
+
+def start_led(self, args: dict
+
+):
+self.setFlashProperties(args)
+self.flashLed()
+self.close()
+
+
+def transform_flash_led_args_input(self):
+    self.start_led_pin = int(input("LED number: "))
+    self.toggle_value = int(input("Toggle on (1) or off (0): "))
+    if self.toggle_value == 1:
+        self.flash_speed = int(input("Set flash speed (1-10): "))
+        self.flash_stop = False
+    else:
+        self.flash_speed = 0
+        self.flash_stop = True
+
+
+def transform_flash_led_arguments_main(self, args: dict
+
+):
+if not args.speed and not args.stop:
+    args.stop = True
+self.toggle_value = args.toggle
+self.flash_speed = args.speed
+self.start_led_pin = args.led
+self.flash_stop = args.stop
+
+
+def toggleLed(self):
+    # this is the non-oop way
+    # pifacedigitalio.digital_write(pin, i)
+    # output_pins and leds seem to be the same array, leds has methods
+    # self.pfio.output_pins[pin].value = i
+    if self.toggle_value == 0:
+        self.pfio.leds[self.start_led_pin].turn_off()
+    else:
+        self.pfio.leds[self.start_led_pin].turn_on()
+        self.pfio.leds[self.start_led_pin].set_high()
+
+
+def setFlashProperties(self, args: dict
+
+):
+self.setFlashSpeedProperties(args)
+self.setSleepTime()
+
+
+def setFlashSpeedProperties(self, args: dict
+
+):
+self.setFlashSpeedLimit()
+self.setFlashHighSpeed()
+self.setFlashLowSpeed()
+if not args:
+    self.transform_flash_led_args_input()
+else:
+    self.transformFlashLedArgsMain(self, args)
+self.transformFlashSpeed()
+
+
+def setFlashHighSpeed(self):
+    self.highSpeed = self.flashSpeedLimit - 1
+
+
+def setFlashLowSpeed(self):
+    self.lowSpeed = 1
+
+
+def setFlashSpeedLimit(self):
+    self.flashSpeedLimit = 11
+
+
+def transformFlashSpeed(self):
+    if self.flash_speed > self.highSpeed:
+        self.flash_speed = self.highSpeed
+    elif self.flash_speed < self.lowSpeed:
+        self.flash_speed = self.lowSpeed
+
+
+def setSleepTime(self):
+    self.sleepTime = self.flashSpeedLimit - self.flash_speed
+
+
+def flashLed(self):
+    self.toggleLed()
+    if self.flash_stop:
+        return
+    sleep(self.sleepTime)
+    self.flipToggle()
+    self.flashLed()
+
+
+def flipToggle(self):
+    if self.toggle_value == 1:
+        self.toggle_value = 0
+    else:
+        self.toggle_value = 1
+
+
+def readInput(self):
+    # pifacedigitalio.digital_read(0)
+    print(self.pfio.output_pins[self.start_led_pin].value)
+
+
+def togglePinPullups(self, pinNumber, pinState):
+    digital_write_pullup(pinNumber, pinState)
+
+
+def close(self):
+    self.pfio.deinit_board()
+
+
+def shutOffAllLeds(self):
+    self.pfio.output_port.all_off()
+
+
+def main():
+    try:
+        args_dict = StartLedArgumentParser().args_dict
         startled = StartLed
-        startled.startLed(startled, args_dict)
-    except pifacedigitalio.core.NoPiFaceDigitalError as e :
+        startled.start_led(startled, args_dict)
+    except pifacedigitalio.core.NoPiFaceDigitalError as e:
         excinfo = sys.exc_info()
         print (e, excinfo.tb_lineno)
         sys.exit(1)
-    except KeyboardInterrupt as ki :
-        startled.startLed(startled, args_dict)
+    except KeyboardInterrupt as ki:
+        startled.start_led(startled, args_dict)
     except ImportError as e:
         print (e)
         sys.exit(1)
-    
-
-if __name__ == '__main__' : main()
 
 
+if __name__ == '__main__':
+    main()
 
 
-    
+
+
+
 
--- StartLedArgumentParser.py
+++ StartLedArgumentParser.py
@@ -2,30 +2,21 @@
 
 import argparse
 
-class StartLedArgumentParser :
-    parser = None
-    args = None
-    args_dict = dict()
 
-    def __init__(self) :
+class StartLedArgumentParser:
+    def __init__(self):
         self.parser = argparse.ArgumentParser()
-        self.parser.add_argument('-l', '--led', action = "store", default = None)
-        self.parser.add_argument('-t', '--toggle', action = "store_true", default = False)
-        self.parser.add_argument('-s', '--speed', action = "store", default = None)
-        self.parser.add_argument('-x', '--stop', action = "store_true", default = False)
+        self.parser.add_argument('-l', '--led', action="store", default=None)
+        self.parser.add_argument('-t', '--toggle', action="store_true",
+                                 default=False)
+        self.parser.add_argument('-s', '--speed', action="store", default=None)
+        self.parser.add_argument('-x', '--stop', action="store_true",
+                                 default=False)
 
-        self.setArgs(self)
-        self.setArgsDict(self)
-        
+    @property
+    def args(self):
+        return self.parser.parse_args()
 
-    def setArgs(self) :
-        self.args = self.parser.parse_args()
-
-    def setArgsDict(self) :
-        self.args_dict = vars(self.args)
-
-    def getArgs(self) :
-        return self.args
-
-    def getArgsDict(self) :
-        return self.args_dict
+    @property
+    def args_dict(self):
+        return vars(self.args)
